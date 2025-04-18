98585f1f02893e11717c0f9e284bb9410d2f289a
ervenus-dott <120420594+ervenus-dott@users.noreply.github.com>
2025-04-17 19:38:13 -0600

    loop over a list of eyes to draw them and don't draw image till its loaded

--- poke_my_eye.js
+++ poke_my_eye.js
@@ -8,36 +8,37 @@ var orangeMagentaBlue = document.getElementById("orange-magenta-blue");
 var orangeYellowBlue = document.getElementById("orange-yellow-blue");
 var redBlob = document.getElementById("red-blob");
 
-context.drawImage(greenNormal, 21, 71);
-// this is the properties for the 4 eyed green blob
+
 var blobProperties = {
     boundingBox: {
-        vertA: [43, 91],
-        vertB: [226, 91],
-        vertC: [226, 260],
-        vertD: [43, 260],
+        topLeft: [43, 91],
+        bottomRight: [226, 260],
     },
-    eyes: {
-        eye0: [165, 139],
-        eye1: [201, 234],
-        eye2: [126, 226],
-        eye3: [68, 136]
-    },
+    eyes: [
+        [165, 139],
+        [201, 234],
+        [126, 226],
+        [68, 136],
+        [300, 136],
+    ],
 };
 var drawBlobCoords = function() {
     context.fillStyle = "blue";
     context.font = "48px serif";
     context.textBaseline = "middle";
     context.textAlign = "center";
-    context.fillText("X", blobProperties.eyes.eye0[0],blobProperties.eyes.eye0[1]);
-    context.fillText("X", blobProperties.eyes.eye1[0],blobProperties.eyes.eye1[1]);
-    context.fillText("X", blobProperties.eyes.eye2[0],blobProperties.eyes.eye2[1]);
-    context.fillText("X", blobProperties.eyes.eye3[0],blobProperties.eyes.eye3[1]);
-    boxWidth = blobProperties.boundingBox.vertB[0] - blobProperties.boundingBox.vertA[0];
-    boxHeight = blobProperties.boundingBox.vertC[1] - blobProperties.boundingBox.vertB[1];
-    context.strokeRect(blobProperties.boundingBox.vertA[0], blobProperties.boundingBox.vertA[1], boxWidth, boxHeight);
+    blobProperties.eyes.forEach(function(eye) {
+        context.fillText("X", eye[0], eye[1]);
+    })
+    boxWidth = blobProperties.boundingBox.bottomRight[0] - blobProperties.boundingBox.topLeft[0];
+    boxHeight = blobProperties.boundingBox.bottomRight[1] - blobProperties.boundingBox.topLeft[1];
+    context.strokeRect(blobProperties.boundingBox.topLeft[0], blobProperties.boundingBox.topLeft[1], boxWidth, boxHeight);
 }
-drawBlobCoords()
+greenNormal.addEventListener("load", function() {
+    context.drawImage(greenNormal, 21, 71);
+    // this is the properties for the 4 eyed green blob
+    drawBlobCoords()
+})
 // context.drawImage(greenThree, 21, 71);
 // context.drawImage(lightBlueMagentaBlue, 21, 71);
 // context.drawImage(lightBlueYellowBlue, 21, 71);
